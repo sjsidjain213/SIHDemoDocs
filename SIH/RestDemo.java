6c0ec39d0a70ae0b840d1f12921c53ba9f358577
sjsidjain213 <sjsidjain213@gmail.com>
2017-03-10 23:18:13 +0530

    Create RestDemo.java

---
+++ SIH/RestDemo.java
@@ -0,0 +1,48 @@
+package SIH;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+@WebServlet("/RestDemo")
+public class RestDemo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RestDemo() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+    private JSONParser parser = new JSONParser();
+    private JSONObject joUser = null;
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	RestDemoBean rdb = new RestDemoBean();
+	rdb.getDocument("sjsidjain");
+	PrintWriter out = response.getWriter();
+  out.println(rdb.getContactInformation());
+	}
+
+}
